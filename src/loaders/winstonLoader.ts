4292621ee03960a49e366a04d0f669c6e45a58be
Alejandro@BEELZEBUB <alejandro@evotechnology.co>
2020-04-15 21:42:05 -0400

    fixes

--- src/loaders/winstonLoader.ts
+++ src/loaders/winstonLoader.ts
@@ -6,12 +6,12 @@
  * Licensed under the MIT license.
  */
 
-import { MicroframeworkLoader, MicroframeworkSettings } from 'microframework-w3tec';
+import { MicroframeworkLoader } from 'microframework-w3tec';
 import { configure, format, transports } from 'winston';
 
 import { env } from '../env';
 
-export const winstonLoader: MicroframeworkLoader = (settings: MicroframeworkSettings | undefined) => {
+export const winstonLoader: MicroframeworkLoader = () => {
     configure({
         transports: [
             new transports.Console({
